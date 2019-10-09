c50ddaf194ad5a9f22d972d13783490cef5640c7
Sraita <sraita@139.com>
2019-10-09 21:49:38 +0800

    code clean

--- webServer/server/api/menu.js
+++
@@ -1,9 +0,0 @@
-// 获取系统资源菜单
-var express = require('express');
-var Router = express.Router();
-
-var MenuController = require('../controller/menu');
-
-Router.route('/').get(MenuController.getList);
-
-module.exports = Router;
